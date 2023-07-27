d101a1542c876e912a61ed171faa7211ee448641
Robert Butler <butler1970@gmail.com>
2023-07-27 10:35:08 -0700

    Last ditch effort to retrieve client ip address via api end point.

--- routes/api.php
+++ routes/api.php
@@ -26,14 +26,13 @@ Route::middleware('auth:sanctum')->get('/lead/find/{id}', [LeadController::class
 
 Route::post('/token/generate', function (Request $request) {
     try {
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
-            $success['token'] =  $user->createToken($request->token_name)->plainTextToken;
-            $success['name'] =  $user->name;
+            $success['token'] = $user->createToken($request->token_name)->plainTextToken;
+            $success['name'] = $user->name;
 
             return $success;
-        }
-        else{
+        } else {
             return ['error' => "Unauthorized"];
         }
 
@@ -43,5 +42,14 @@ Route::post('/token/generate', function (Request $request) {
 });
 
 Route::get('/get_ip_address', function (Request $request) {
-    return file_get_contents('https://api.ipify.org');
+    // if user from the share internet
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } //if user is from the proxy
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } //if user is from the remote address
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
 });
