b7ad4938ae44b85574064a6393d89e0ec1f19bd3
Robert Butler <butler1970@gmail.com>
2023-08-17 15:08:30 -0700

    Refactoring tests.

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -5,18 +5,8 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ApiTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/info');
-
-        $response->assertStatus(200);
-    }
-
     public function test_ip_location_works(): void {
         $response = $this->post('/api/ip/location', ['ip' => '68.224.134.148']);
 
